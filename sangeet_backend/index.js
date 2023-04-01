d7efbc6d86259c41bb189cf6b848dbadcc84d315
Abhimanyu Kumar <abhimanyukumar1025@gmail.com>
2023-04-01 12:40:19 +0530

    connected nodejs with mongo and used dotenv

--- sangeet_backend/index.js
+++ sangeet_backend/index.js
@@ -1,7 +1,25 @@
 const express= require("express");//import all express funtionalities
+const mongoose =require ("mongoose");
+require("dotenv").config();
 const app=express();//we bring all package code in our local file
+const port =8080;
+mongoose.connect(  
 
-const port =8000;
+"mongodb+srv://abhimanyukumar19987:"+process.env.MONGO_PASSWORD+"@cluster0.d0rguwk.mongodb.net/?retryWrites=true&w=majority" ,
+   {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+   }
+)
+
+
+.then ((x)=>{
+    console.log("Connected to  Mongo!!");
+})
+
+.catch((err)=>{
+    console.log("Error while connecting to Mongo");
+});
 
 app.get("/",(req,res)=>{
     res.send("Hello World");
@@ -9,4 +27,5 @@ app.get("/",(req,res)=>{
 
 app.listen(port,()=>{
     console.log ("App is runing on port "+port);
+  
 }); 
