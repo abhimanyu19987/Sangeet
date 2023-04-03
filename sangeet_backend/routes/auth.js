39d975088e8d7c107f09db1284133a0704f03e31
Abhimanyu Kumar <abhimanyukumar1025@gmail.com>
2023-04-03 15:33:08 +0530

    Login User in ExpressJS

--- sangeet_backend/routes/auth.js
+++ sangeet_backend/routes/auth.js
@@ -32,4 +32,30 @@ router.post("/register",async(req,res)=>{
     delete userToReturn.password;
     return res.status(200).json(userToReturn);
 });
+
+
+router.post("/login",async(req,res)=>{
+
+
+    const {email,password}=req.body ;
+
+
+    const user=await User.findOne({emai:email});
+
+    if(!user){
+        return res.status(403).json({err:"Invalid credentials"});
+    }
+//  password is store in hash format so we use bycrpt compare 
+    const isPasswordValid =await bycrpt.compare(password,user.password);
+
+
+
+    if(!isPasswordValid){
+        return res.status(403).json({err:"Invalid credentials"});
+    }
+    const token =await getToken(user.email,user);
+    const userToReturn ={...newUser.toJSON(),token};
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+});
 module .exports =router;
